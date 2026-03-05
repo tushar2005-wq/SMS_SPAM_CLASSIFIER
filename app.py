537cd22e0de5d831ec3a3b692b868e801113ea24
Tushar <srivastavatusar548@gmail.com>
2026-03-05 23:25:29 +0530

    fix nltk punkt_tab deployment

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if st.button('Predict'):
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
+    vector_input = tfidf.transform([transformed_sms]).toarray()
     # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
